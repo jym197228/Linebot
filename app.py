75739adfe9b18b5ace4336e3815d8628badcf74b
Chris Chung <jym197@gmail.com>
2020-09-20 16:36:39 +0800

    新增訂位對話，以及刪除記事本內容

--- app.py
+++ app.py
@@ -38,12 +38,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '我看不懂你想要幹嘛?'
     
+    r = '我看不懂你想要幹嘛?'
     if msg in ['hi', 'Hi', 'HI', '嗨']:
         r = '嗨'
     elif msg == '你想要出去玩嗎?':
         r = '我不想要喔！'
+    elif '訂位' in msg:
+        r = '您想要訂位嗎?'
     
     line_bot_api.reply_message(
         event.reply_token,
